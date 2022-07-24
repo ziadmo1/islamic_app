c267d6bf527280ef1ba3b9671db22f6f4cf5468b
islamic_app <ziadmo703@gmail.com>
2022-07-24 18:52:20 +0200

    add splash screen.

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       initialRoute: HomePage.routeName,
       routes: {
         HomePage.routeName : (_)=>HomePage()
